837032e24688c2842a80b603302b82f27006b317
abhisheksaini <saini.abhishek@hotmail.com>
2018-06-17 13:55:06 +0200

    navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SettingLoadingComponent } from './setting-loading/setting-loading.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NavigatingComponent } from './navigating/navigating.component';
 
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'registering', component: SettingLoadingComponent},
+  { path: 'navigation', component: NavigatingComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingLoadingComponent,
+    HomeComponent,
+    NavigatingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
